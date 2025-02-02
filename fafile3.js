ae59aaa2dace948af52c17ac6f64dc0c21e6a89a
PhillYahzeeBSia <phillyahzee.sia@smc.pshs.edu.ph>
2025-02-02 22:36:38 +0800

    Add files via upload

---
+++ fafile3.js
@@ -0,0 +1,16 @@
+function myFunction() {
+  if (confirm("Do you agree to share your personal information with my site?")) {
+    const nickname = "Yahzee";
+    const height = "5\'5\"";
+    const weight = "180.779 lbs";
+
+    const total = " Name: " + nickname + "\n Height: " + height + "\n Weight: "+ weight;
+    console.log(total);
+  } 
+  else {
+    console.log("User does not wish to share his/her information.");
+  }
+}
+
+    
+    
