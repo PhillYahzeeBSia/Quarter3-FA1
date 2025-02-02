e596f555852e3b4c429dad989f43e6ef72a4580d
PhillYahzeeBSia <phillyahzee.sia@smc.pshs.edu.ph>
2025-02-02 20:29:15 +0800

    Add files via upload

---
+++ fafile1.js
@@ -0,0 +1,6 @@
+const nickname = "Yahzee";
+const height = "5\'5\"";
+const weight = "180.779 lbs";
+
+const total = " Name: " + nickname + "\n Height: " + height + "\n Weight: "+ weight;
+alert(total);
